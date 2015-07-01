18220e6493f77290e7ef24ea792aed345a3feef1
Kimpe Andy <andykimpe@gmail.com>
2015-07-01 11:07:54 +0200

    Create plowdel.bat for windows batch integration
    Create plowdel.bat for windows batch integration

---
+++ src/plowdel.bat
@@ -0,0 +1,8 @@
+@ echo off
+if %PROCESSOR_ARCHITECTURE%==x86 (
+set cygwininstalldir=C:\cygwin
+) else (
+set cygwininstalldir=C:\cygwin64
+)
+set PATH=%PATCH%;%cygwininstalldir%\bin
+bash /usr/share/plowshare/delete.sh %*
